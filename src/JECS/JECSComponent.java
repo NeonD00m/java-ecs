e3f0e8e9eb5ae5baa1bbe41ea2ef074974410a67
NeonD00m <neond00m.business@gmail.com>
2023-12-01 15:44:47 -0800

    fixed className()

--- src/JECS/JECSComponent.java
+++ src/JECS/JECSComponent.java
@@ -1,5 +1,8 @@
 package JECS;
 
 public class JECSComponent {
-    public String className() {return this.getClass().getName();}
+    public String className() {
+        String[] split = this.getClass().getName().split("\\.");
+        return split[split.length - 1];
+    }
 }
