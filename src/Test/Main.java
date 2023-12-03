36cd4b2401ae064a549fe68ccff3fa6fc00e915e
NeonD00m <neond00m.business@gmail.com>
2023-12-03 09:54:06 -0800

    updated Main to test more functionality

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -8,13 +8,53 @@ import JECS.ECSInterface;
 public class Main {
     public static void main(String[] args) {
         JECS game = new JECS();
-        int testId = game.spawn(new JECSComponent[]{new StringComponent()});
+        int testId = game.spawn(new JECSComponent[]{
+                new StringComponent("Tree"),
+                new HealthComponent(5),
+        });
+        System.out.println("LJ ID: " + game.spawn(new JECSComponent[]{
+                new LumberjackComponent()
+        }));
+        game
+//                .addSystem(world ->
+//                    System.out.println("printing component: " +
+//                        ((StringComponent) world.get(testId, new String[]{"StringComponent"})[0]).field
+//                ))
+                .addSystem(world -> {
+                    LumberjackComponent person = (LumberjackComponent) world.getSingle(new String[]{"LumberjackComponent"})[0];
+                    if (!world.contains(person.treeId)) {
+                        // find a tree
+                        person.treeId = world.getSingleByIndex(new String[]{"StringComponent", "HealthComponent"});
 
-        game.addSystem(world ->
-                System.out.println("printing component: " +
-                        ((StringComponent) world.get(testId, new String[]{"StringComponent"})[0]).field
-                ))
-                .addSystem(world -> System.out.println(world.query(new String[]{}).length + " entities in the world currently."))
+                        // if no tree, quit
+                        if (person.treeId == -1) {
+                            world.quit();
+                            System.out.println("quit simulation");
+                        } else {
+                            System.out.println("lumberjack found a tree");
+                        }
+                    } else {
+                        if (person.stepsToWait == 0) {
+                            // cut down the tree
+                            HealthComponent treeHealth = (HealthComponent) world.get(person.treeId, new String[]{"HealthComponent"})[0];
+                            treeHealth.health--;
+                            person.stepsToWait = 60;
+                            if (treeHealth.health == 0) {
+                                world.despawn(person.treeId);
+                                System.out.println("the lumberjack cut down the tree!");
+                            } else {
+                                System.out.println("lumberjack axed the tree (Health: " + treeHealth.health + ")");
+                            }
+                        } else {
+                            // muster up the energy
+                            person.stepsToWait--;
+                            if (person.stepsToWait == 30) {
+                                System.out.println("About to attempt to chop down the tree again!");
+                            }
+                        }
+                    }
+                })
+//                .addSystem(world -> System.out.println(world.query(new String[]{}).length + " entities in the world currently."))
                 .startLoop();
     }
 }
