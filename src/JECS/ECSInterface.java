aef7e11e0179095e8a658ae31b5e9f49fc556a57
NeonD00m <neond00m.business@gmail.com>
2023-12-12 07:44:22 -0800

    moved methods around for organization

--- src/JECS/ECSInterface.java
+++ src/JECS/ECSInterface.java
@@ -1,16 +1,16 @@
 package JECS;
 
 public interface ECSInterface {
+    int spawn(JECSComponent[] list);
+    int spawnAt(int id, JECSComponent[] list);
     boolean contains(int id);
+    void insert(int entityId, JECSComponent componentInstance);
     void despawn(int id);
     void remove(int id, String componentName);
     JECSComponent[][] query(String[] components);
     JECSComponent[] get(int id, String[] components);
     JECSComponent[] getSingle(String[] components);
     int getSingleByIndex(String[] components);
-    int spawn(JECSComponent[] list);
-    int spawnAt(int id, JECSComponent[] list);
-    void insert(int entityId, JECSComponent componentInstance);
     ECSInterface addSystem(ECSSystem system);
     void startLoop();
     void quit();
